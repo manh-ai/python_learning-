9367b7436b1ecf28e8eafb690ca795e43902247c
manh-ai <sonofthegod07@gmail.com>
2026-06-25 21:32:20 +0700

    Add files via upload

---
+++ while_loop_food.py
@@ -0,0 +1,6 @@
+food = input("nhap mon ban thich (nhap q de dung)")
+while not food == "q":
+    print(f"ban thich an {food}")
+    food = input("nhap mon ban thich (nhap q de dung)")
+
+print("bye bye")
